bc4a600975825e970d6eb03a245e5be99dccc845
knfjed <knfjed@gmail.com>
2020-03-06 18:22:35 +0900

    create form

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var express = require("express");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
 var app = express();
 
 // テンプレートを読み込むための設定
@@ -11,6 +12,9 @@ app.set("view engine", "ejs");
 
 // middleware
 // 静的なファイル名でアクセスされたら、ここから配信
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(logger("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
@@ -18,6 +22,36 @@ app.use((req, res, next) => {
   next();
 });
 
+// app.param : パラメーターに共通の処理をさせる
+app.param("id", (req, res, next, id) => {
+  var users = [
+    "suzuki",
+    "ono",
+    "sato",
+    "sugishita",
+    "fujieda",
+    "kaneko",
+    "abe"
+  ];
+  req.params.name = users[id];
+  next();
+});
+app.get("/hello/:id", (req, res) => {
+  res.send("hello, " + req.params.name);
+});
+app.get("/bye/:id", (req, res) => {
+  res.send("bye, " + req.params.name);
+});
+
+// newにアクセスすると、newというテンプレートを表示
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+app.post("/create", (req, res) => {
+  res.send(req.body.name);
+});
+
+// ejsテンプレートに値を与える
 app.get("/", (req, res) => {
   res.render("index", { title: "title" });
 });
